ac883efd51165dc9e405ffea08d10cfde1f334c4
josh114 <freejosh02@gmail.com>
2023-12-14 05:33:53 +0100

    update frontend with navbar and more layout

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,8 +5,18 @@ import banner from '../assets/library3.jpg';
 const Home = () => {
   return (
     <Flex w={'100%'}>
-      <Flex width={'100%'} h={'250px'}>
-        <Image src={banner} objectFit={'cover'} />
+      <Flex width={'100%'} h={'280px'}>
+        <Image src={banner} objectFit={'cover'} mt={'8px'} />
+      </Flex>
+      <Flex
+        w={'300px'}
+        minH={'100%'}
+        justify={'center'}
+        border={'1px solid'}
+        borderLeftColor={'#eaeaea'}
+        borderTopColor={'white'}
+      >
+        <Text>Recommended</Text>
       </Flex>
     </Flex>
   );
