0241717da325961b6a8503ac00311c3ed7417121
josh114 <freejosh02@gmail.com>
2023-12-19 10:27:43 +0100

    update cors and auth

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import cors from 'cors';
-import { logger } from './middleware/logger.js';
-import corsOptions from './config/corsOptions.js';
-import userRouter from './src/user/userRoutes.js';
-import authRouter from './src/auth/authRoutes.js';
-import novelRouter from './src/novel/novelRouter.js';
+import express from "express";
+import cors from "cors";
+import { logger } from "./middleware/logger.js";
+import corsOptions from "./config/corsOptions.js";
+import userRouter from "./src/user/userRoutes.js";
+import authRouter from "./src/auth/authRoutes.js";
+import novelRouter from "./src/novel/novelRouter.js";
 const app = express();
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(logger);
 app.use(express.json());
 
@@ -14,10 +14,10 @@ app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
-app.use('/files', express.static('files'));
+app.use("/files", express.static("files"));
 
-app.use('/user', userRouter);
-app.use('/auth', authRouter);
-app.use('/novel', novelRouter);
+app.use("/user", userRouter);
+app.use("/auth", authRouter);
+app.use("/novel", novelRouter);
 
 export default app;
