67e5af76aa3a1ea5fab7baaffc133d018616738e
root <joshua@luday.se>
2024-01-11 12:02:45 +0100

    update chapter api to handle request on slug and id

--- backend/src/novel/chapter.js
+++ backend/src/novel/chapter.js
@@ -1,19 +1,20 @@
-import Updates from '../updates/updatesModel.js';
-import { Chapter, Novel } from './novelModel.js';
+import Updates from "../updates/updatesModel.js";
+import { Chapter, Novel } from "./novelModel.js";
+import mongoose from "mongoose";
 
 export const createChapter = async (req, res) => {
   try {
     const { chapter, novel, content } = req.body;
     if (!chapter || !content || !novel) {
       res.status(400).json({
-        message: 'chapter number and content as well as novel id is required',
+        message: "chapter number and content as well as novel id is required",
       });
       return;
     }
     const nov = await Novel.findById(novel);
     if (!nov) {
       res.status(400).json({
-        message: 'novel not found',
+        message: "novel not found",
       });
       return;
     }
@@ -21,8 +22,8 @@ export const createChapter = async (req, res) => {
     if (!nov.slug) {
       slug = `${nov.name
         .toLowerCase()
-        .split(' ')
-        .join('-')}-chapter-${chapter}`;
+        .split(" ")
+        .join("-")}-chapter-${chapter}`;
     } else {
       slug = `${nov.slug}-chapter-${chapter}`;
     }
@@ -35,7 +36,7 @@ export const createChapter = async (req, res) => {
     const update = {
       slug,
       novel: nov.name,
-      chapter: chapter
+      chapter: chapter,
     };
     await Updates.create(update);
     const chap = await Chapter.create(body);
@@ -48,7 +49,7 @@ export const createChapter = async (req, res) => {
 export const getChapters = async (req, res) => {
   try {
     const { novel } = req.params;
-    const chaps = await Chapter.find({ novel }).populate('novel');
+    const chaps = await Chapter.find({ novel }).populate("novel");
     res.status(200).json(chaps);
   } catch (error) {
     res.status(404).json({ message: error?.message });
@@ -57,13 +58,25 @@ export const getChapters = async (req, res) => {
 
 export const getChapter = async (req, res) => {
   try {
+    let chap;
+    let count;
     const { id } = req.params;
-    const chap = await Chapter.findById(id).populate('novel');
+    if (mongoose.Types.ObjectId.isValid(id)) {
+      chap = await Chapter.findById(id).populate("novel").exec();
+      count = await Chapter.countDocuments({ novel: chap.novel });
+    } else {
+      chap = await Chapter.findOne({ slug: id }).populate("novel").exec();
+      count = await Chapter.countDocuments({ novel: chap.novel });
+    }
     if (!chap) {
-      res.status(404).json({ message: 'chapter not found' });
+      res.status(404).json({ message: "chapter not found" });
       return;
     } else {
-      res.status(200).json(chap);
+      const chapter = {
+        count: count,
+        ...chap.toObject(),
+      };
+      res.status(200).json(chapter);
     }
   } catch (error) {
     res.status(400).json({ message: error?.message });
@@ -75,7 +88,7 @@ export const updateChapter = async (req, res) => {
     const { id } = req.params;
     const exist = await Chapter.findById(id);
     if (!exist) {
-      res.status(404).json({ message: 'chapter not found' });
+      res.status(404).json({ message: "chapter not found" });
       return;
     }
     const chap = await Chapter.findByIdAndUpdate(id, req.body, { new: true });
@@ -89,7 +102,7 @@ export const deleteChapter = async (req, res) => {
     const { id } = req.params;
     const exist = await Chapter.findById(id);
     if (!exist) {
-      res.status(404).json({ message: 'chapter not found' });
+      res.status(404).json({ message: "chapter not found" });
       return;
     }
     await Chapter.findByIdAndDelete(id);
